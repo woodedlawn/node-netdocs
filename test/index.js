fa891bedea87429f6497084941b6a0836eaca8aa
jonathan <jonathan@woodedlawn.com>
2013-02-05 11:06:07 -0800

    first running test for CI

---
+++ test/index.js
@@ -0,0 +1,11 @@
+var should = require('should');
+var nd = require('../lib/index');
+
+describe('nd', function() {
+    describe('with no arguments', function() {
+        it('returns an empty array', function() {
+            var result = nd();
+            result.should.eql([]);
+        });
+    });
+});
